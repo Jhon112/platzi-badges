8c04c00421b86adb3ee3fb60ee36695f0c1cb1c3
Jhon Arias <jhonarmont@hotmail.com>
2019-11-06 21:48:01 -0500

    Add classnames for styles Use prettier to format code. Import conf logo and css file

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,29 +1,35 @@
 import React from "react";
+import "./styles/Badge.css";
+import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <img src="" alt="Logo de la conferencia"/>
-                </div>
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+}
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://source.unsplash.com/random"
+            alt="Avatar"
+          />
+          <h1>
+            Jhon <br /> Arias
+          </h1>
+        </div>
 
-                <div>
-                    <img src="https://source.unsplash.com/random" alt="Avatar"/>
-                    <h1>
-                        Jhon <br/> Arias
-                    </h1>
-                </div>
+        <div className="Badge__section-info">
+          <h3>Full stack developer</h3>
+          <div>@jhonalexander11</div>
+        </div>
 
-                <div>
-                    <h3>Full stack developer</h3>
-                    <div>@jhonalexander11</div>
-                </div>
-
-                <div className="Badge__footer">#platziconf</div>
-            </div>
-        );
-    }
+        <div className="Badge__footer">#platziconf</div>
+      </div>
+    );
+  }
 }
 
-export default Badge
+export default Badge;
