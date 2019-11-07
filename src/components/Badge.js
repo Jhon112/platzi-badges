e025ab969718da334f5f92b9d15f24c86b7ef120
Jhon Arias <jhonarmont@hotmail.com>
2019-11-06 21:42:38 -0500

    Add images needed for project Update Badge component.
    It has now info about participant

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -4,7 +4,23 @@ class Badge extends React.Component {
     render() {
         return (
             <div>
-                "Hola mundo"
+                <div>
+                    <img src="" alt="Logo de la conferencia"/>
+                </div>
+
+                <div>
+                    <img src="https://source.unsplash.com/random" alt="Avatar"/>
+                    <h1>
+                        Jhon <br/> Arias
+                    </h1>
+                </div>
+
+                <div>
+                    <h3>Full stack developer</h3>
+                    <div>@jhonalexander11</div>
+                </div>
+
+                <div className="Badge__footer">#platziconf</div>
             </div>
         );
     }
