dd18157bd73cf4b6258b354ec41dd41955b9fea8
Jhon Arias <jhonarmont@hotmail.com>
2019-11-07 00:55:47 -0500

    Set state of component
    Create the initial state, add method to handle onChange.
    
    Disrupt state on render method and use vars on values  to control inputs

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,36 +2,89 @@ import React from "react";
 
 class BadgeForm extends React.Component {
 
+	state = {
+		firstName: '',
+		lastName: '',
+		twitter: '',
+		jobTitle: '',
+		email: ''
+	}
+
 	formSubmitted = e => {
 		e.preventDefault();
-		console.log("Submitted");
-	}
+		let {state} = this
+
+		console.log(state)
+	};
+
+	inputChanged = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 
 
   render() {
+
+		let {firstName,
+		lastName,
+		twitter,
+		jobTitle,
+		email} = this.state;
+
     return (
       <div>
         <h1>New Attendant</h1>
         <form onSubmit={this.formSubmitted}>
           <div className="form-group">
             <label>First Name</label>
-            <input className="form-control" type="text" name="firstName" />
+            <input
+              onChange={this.inputChanged}
+              className="form-control"
+              type="text"
+              name="firstName"
+              value={firstName}
+            />
           </div>
           <div className="form-group">
             <label>Last Name</label>
-            <input className="form-control" type="text" name="lastName" />
+            <input
+              onChange={this.inputChanged}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={lastName}
+            />
           </div>
           <div className="form-group">
             <label>Email</label>
-            <input className="form-control" type="email" name="email" />
+            <input
+              onChange={this.inputChanged}
+              className="form-control"
+              type="email"
+              name="email"
+              value={email}
+            />
           </div>
           <div className="form-group">
             <label>Job Title</label>
-            <input className="form-control" type="text" name="jobTitle" />
+            <input
+              onChange={this.inputChanged}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={jobTitle}
+            />
           </div>
           <div className="form-group">
             <label>Twitter</label>
-            <input className="form-control" type="text" name="twitter" />
+            <input
+              onChange={this.inputChanged}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={twitter}
+            />
           </div>
           <button className="btn btn-primary">Save</button>
         </form>
