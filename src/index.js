51b331832260c9eb8eba368c916d14d403c33405
Jhon Arias <jhonarmont@hotmail.com>
2019-11-06 21:36:11 -0500

    Import Badge component, and render it on container

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Badge from './components/Badge'
+import './global.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+let container = document.getElementById("app")
+ReactDOM.render(<Badge />, container);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
