f756cbf11b627334287c8b08f08ed0d997463fa8
Jhon Arias <jhonarmont@hotmail.com>
2019-11-07 00:17:09 -0500

    Create BadgeNew component.
    This components render the navbar, it has also a hero.
    
    Use bootstrap to do the grid system. Render Badge in a 6 columns div

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import header from "../images/badge-header.svg";
 import  './styles/BadgeNew.css'
 
+class BadgeNew extends React.Component {
+    render() {
+        return (
+          <div>
+            <Navbar />
+            <div className="BadgeNew__hero">
+              <img className="img-fluid" src={header} alt="Hero" />
+            </div>
 
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <Badge />
+                </div>
+                <div className="col-md-6">
+                    Form
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default BadgeNew;
