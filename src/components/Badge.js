bb938b9205cc9721050e516ac1d03e1288437346
Jhon Arias <jhonarmont@hotmail.com>
2019-11-06 21:36:20 -0500

    Create Badge component.

---
+++ src/components/Badge.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Badge extends React.Component {
+    render() {
+        return (
+            <div>
+                "Hola mundo"
+            </div>
+        );
+    }
+}
+
+export default Badge
