79db1acc0781ee5a222689c05ef1cec2c18d03bc
Jhon Arias <jhonarmont@hotmail.com>
2019-11-07 00:32:10 -0500

    Update BadgeForm component.
    Add form groups with labels and inputs. Add name to each input

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,36 @@
-import React from 'react'
-
+import React from "react";
 
 class BadgeForm extends React.Component {
-    render() {
-        return ("Form")
-    };
+  render() {
+    return (
+      <div>
+        <h1>New Attendant</h1>
+        <form action="">
+          <div className="form-group">
+            <label>First Name</label>
+            <input className="form-control" type="text" name="firstName" />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input className="form-control" type="text" name="lastName" />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input className="form-control" type="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label>Job Title</label>
+            <input className="form-control" type="text" name="jobTitle" />
+          </div>
+          <div className="form-group">
+            <label>Twitter</label>
+            <input className="form-control" type="text" name="twitter" />
+          </div>
+          <button className="btn btn-primary">Save</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default BadgeForm;
