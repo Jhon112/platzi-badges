3147a8fc3acfe5849af76afe72c1a1b1e4689382
Jhon Arias <jhonarmont@hotmail.com>
2019-11-07 00:22:32 -0500

    Send props to Badge component. Fix identation

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Badge from '../components/Badge';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
 import header from "../images/badge-header.svg";
-import  './styles/BadgeNew.css'
+import "./styles/BadgeNew.css";
 
 class BadgeNew extends React.Component {
-    render() {
-        return (
-          <div>
-            <Navbar />
-            <div className="BadgeNew__hero">
-              <img className="img-fluid" src={header} alt="Hero" />
-            </div>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Hero" />
+        </div>
 
-            <div className="container">
-              <div className="row">
-                <div className="col-md-6">
-                  <Badge />
-                </div>
-                <div className="col-md-6">
-                    Form
-                </div>
-              </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <Badge
+                firstName="Jhon"
+                lastName="Jhon"
+                twitter="jhonalexander11"
+                titleJob="Full stack developer"
+              />
             </div>
+            <div className="col-md-6">Form</div>
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BadgeNew;
